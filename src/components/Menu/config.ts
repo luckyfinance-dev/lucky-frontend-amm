ef1ddd5f1ee9591adf857891271ab7f764d0d1f6
luckyfinance-dev <78893395+luckyfinance-dev@users.noreply.github.com>
2021-02-26 15:52:29 -0800

    notes for team

--- src/components/Menu/config.ts
+++ src/components/Menu/config.ts
@@ -68,7 +68,7 @@ const config: MenuEntry[] = [
       },
       {
         label: "Docs",
-        href: "https://lucky-financial.gitbook.io/lucky-financial/",
+        href: "https://docs.lucky.financial",
       },
       {
         label: "Blog",
--- src/constants/GFTPriceContracts.ts
+++ src/constants/GFTPriceContracts.ts
@@ -1,7 +1,9 @@
 const priceContracts: {cakeAddress: string, busdAddress: string, lpAddress:string} = {
   cakeAddress: '0xf952fc3ca7325cc27d15885d37117676d25bfda6',
+  /** cake address actually has goose Egg address in there. */
   busdAddress: '0xe9e7cea3dedca5984780bafc599bd69add087d56',
+  /** binance pegged bUSD Token */
   lpAddress: '0x19e7cbecdd23a16dfa5573df54d98f7caae03019'
+  /** GoldenGoose and BUSD LP - owner is packake swap deployer */
 }
-
 export default priceContracts
